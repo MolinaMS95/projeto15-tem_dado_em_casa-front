0e1c0f909947c468a5f07b57b0fb4de91ab71ed8
Rafhael <rafhael.cancado@gmail.com>
2022-11-25 23:49:18 -0300

    feat: add product get(bugged)/product display

--- src/constants/links.js
+++ src/constants/links.js
@@ -1,2 +1,3 @@
 export const signUpURL = "https://tem-dado-em-casa-api.onrender.com/sign-up";
 export const loginURL = "https://tem-dado-em-casa-api.onrender.com/sign-in";
+export const productsURL = "https://tem-dado-em-casa-api.onrender.com/products";
